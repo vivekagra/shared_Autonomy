ad8c697a85d57df01a876a527e02cff9792d623b
vivekagra <vivekagr@iitk.ac.in>
2019-06-28 02:44:17 +0530

    shared_autonomy

--- shared_autonomy_ws/build/catkin_generated/setup_cached.sh
+++ shared_autonomy_ws/build/catkin_generated/setup_cached.sh
@@ -8,6 +8,8 @@
 
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/vivek/Desktop/Vivek/shared_Autonomy/shared_autonomy_ws/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/vivek/Desktop/Vivek/shared_Autonomy/shared_autonomy_ws/devel/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/vivek/Desktop/Vivek/shared_Autonomy/shared_autonomy_ws/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD="/home/vivek/Desktop/Vivek/shared_Autonomy/shared_autonomy_ws/build"
-export ROSLISP_PACKAGE_DIRECTORIES="/home/vivek/Desktop/Vivek/shared_Autonomy/shared_autonomy_ws/devel/share/common-lisp"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/vivek/Desktop/Vivek/shared_Autonomy/shared_autonomy_ws/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
 export ROS_PACKAGE_PATH="/home/vivek/Desktop/Vivek/shared_Autonomy/shared_autonomy_ws/src:$ROS_PACKAGE_PATH"
---
+++ shared_autonomy_ws/src/train_network/src/train_network.py
@@ -0,0 +1,7 @@
+import gym
+import numpy as np 
+import time
+import rospy
+import roslib
+import q_learn
+# import ros environment
